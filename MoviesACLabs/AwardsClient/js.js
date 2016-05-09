9e1a2aff132b5caf51983dc462aa53c31efe5414
Denis <denis.tdr@gmail.com>
2016-05-09 16:38:32 +0300

    Awards
    Awards

---
+++ MoviesACLabs/AwardsClient/js.js
@@ -0,0 +1,44 @@
+﻿
+
+var app = angular.module('awards', []);
+
+app.service("AwardsService", function ($http) {
+    this.getAwards = function () {
+        return $http({
+            method: "GET",
+            url: "/api/Awards"
+        });
+    };
+    this.addAward = function (theAward) {
+        return $http.post("/api/Awards", theAward);
+    };
+
+    this.deleteAward = function (awardId) {
+        return $http.delete("/api/Awards/" + awardId);
+    };
+});
+
+
+
+app.controller('AwardsController', function ($scope, AwardsService) {
+    $scope.awards = null;
+    $scope.editingAward = null;
+
+    AwardsService.getAwards().then(function (data) {
+        $scope.awards = data.data;
+    });
+
+    $scope.editAward = function (awardId) {
+        $scope.editingAward = findAwardById(awardId);
+    };
+
+
+    var findAwardById = function (awardId) {
+        for (var i = 0; i < $scope.awards.length; i++)
+            if ($scope.awards[i].Id == awardId) {
+                return $scope.awards[i];
+            }
+        return null;
+    };
+
+});
---
+++ MoviesACLabs/Controllers/AwardsController.cs
@@ -0,0 +1,86 @@
+﻿using MoviesACLabs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MoviesACLabs.Controllers
+{
+    public class AwardsController:ApiController
+    {
+        private static List<AwardModel> hardcodedAwards = null;
+        private int lastId = 0;
+
+        public AwardsController()
+        {
+            if(hardcodedAwards == null)
+            {
+                hardcodedAwards = new List<AwardModel>(new[] {
+                    new AwardModel { Id = 0 , Name="Nume1"},
+                    new AwardModel { Id = 1 , Name="Nume2"},
+                    new AwardModel { Id = 2 , Name="Nume3"},
+                    new AwardModel { Id = 3 , Name="Nume4"}
+                });
+                lastId = 3;
+            }
+        }
+        public IList<AwardModel> GetAwards()
+        {
+            return hardcodedAwards;
+        }
+
+        [Route("myAward/{id}")]
+        public AwardModel GetAward(int id)
+        {
+            var award = hardcodedAwards.FirstOrDefault(aw => aw.Id == id);
+            return award;
+        }
+
+        public IHttpActionResult PostAward(AwardModel award)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            award.Id = ++lastId;
+            hardcodedAwards.Add(award);
+            return Ok();
+        }
+
+        public IHttpActionResult PutAward(AwardModel award)
+        {
+            var existingAward = hardcodedAwards.FirstOrDefault(aw => aw.Id == award.Id);
+            if (existingAward == null)
+            {
+                return NotFound();
+            }
+            CopyProperties(award, existingAward);
+            return Ok();
+        }
+
+        public IHttpActionResult DeleteAward(int id)
+        {
+            var toDelete = hardcodedAwards.FirstOrDefault(aw => aw.Id == id);
+            if(toDelete != null)
+            {
+                hardcodedAwards.Remove(toDelete);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        private void CopyProperties(object source, object target)
+        {
+            foreach(var prop in source.GetType().GetProperties())
+            {
+                object defaultValue = prop.PropertyType.IsValueType ? Activator.CreateInstance(prop.PropertyType) : null;
+                if(prop.GetValue(source) != defaultValue)
+                {
+                    prop.SetValue(target, prop.GetValue(source));
+                }
+            }
+        }
+    }
+}
